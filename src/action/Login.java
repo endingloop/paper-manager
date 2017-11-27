c6c723c275560a570e7bfc317eb78f500759ade2
Zhu Siyuan <874333058@qq.com>
2017-11-27 20:27:18 +0800

    修改之后能够刷新页面

--- src/action/Login.java
+++ src/action/Login.java
@@ -3,17 +3,23 @@ package action;
 import java.sql.SQLException;
 
 import model.User;
+import service.Dao;
 import support.UserSupport;
 
-public class Login extends UserSupport{
+public class Login extends UserSupport {
 
 	public String execute() throws SQLException {
-		User user = findUser(getUsername(), getPassword());
-		if(user != null) {
-			setUser(user);
+		
+		if (getUser() != null) {
+			setUser(Dao.findUser(getUser().getUsername())); //不加这一句，页面不刷新 :(
 			return SUCCESS;
 		}
-		else {
+
+		User user = findUser(getUsername(), getPassword());
+		if (user != null) {
+			setUser(user);
+			return SUCCESS;
+		} else {
 			return INPUT;
 		}
 	}
